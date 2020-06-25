4872d60bc2df65e01d8f86632919e3cafbcd3ceb
Summer Healey <summerleigh.healey@gmail.com>
2020-06-25 18:11:39 -0400

    Created currentWeather function to collect and display current weather data for a specific city. Added needed variables and outline to generate five day forecast.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 $(document).ready(() => {
   //Variables
   //  var apiKey = '236f61cf86e4cbb6234939b6635ebc9c';
- 
+  
 
   //SEARCH BUTTON
   /*Description: Button, that once pressed, provides information on city input into the search form to be displayed as current weather and five day forecast*/
 
   $("#searchBtn").on("click", function (event) {
-    event.preventDefault();
     
+    event.preventDefault();
     var city = $("#search-term").val();
     console.log(city);
 
@@ -21,13 +21,50 @@ $(document).ready(() => {
 
   function currentWeather(city) {
     $.ajax({
-      
+      method: "GET",
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=236f61cf86e4cbb6234939b6635ebc9c&units=imperial`,
+      dataType: "json",
+      success: function (response) {
+        console.log(response);
+
+        //currentWeather Variables
+        var currentDateTime = moment().format("LLL");
+        var cityName = response.name;
+        var currentTemperature = Math.round(response.main.temp);
+        var currentHumidity = Math.round(response.main.humidity);
+        var currentWindSpeed = Math.round(response.wind.speed);
+
+        $("#cityDate").html(
+          "<h2>" + cityName + " (" + currentDateTime + ")" + "<h2>"
+        );
+        $("#currentIcon").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            response.weather[0].icon +
+            "@2x.png"
+        );
+        $("#temp").text(currentTemperature + 'F');
+        $("#humidity").text(currentHumidity + "%");
+        $("#windSpeed").text(currentWindSpeed + " MPH");
+
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+
+        forecastWeather(lat, lon);
+      },
+    });
+  }
 
   // forecastWeather FUNCTION
   /*Description: Function collects weather data forecast from the next five days for place selected and diplays in a separate card on page per forecasted day*/
-  function forecastWeather() {
-    
-        //uvIndex DISPLAY
+  function forecastWeather(lat, lon) {
+    $.ajax({
+      method: "GET",
+      url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=236f61cf86e4cbb6234939b6635ebc9c&units=imperial`,
+      dataType: "json",
+      success: function (response) {
+        console.log(response);
+
         
         //fiveDayForecast DISPLAY
         
@@ -39,10 +76,14 @@ $(document).ready(() => {
   /*Description: Displays recent searches as buttons in a list*/
   function searchHistoryDisplay() {
     
+
+    //Displays last search if a search has been conducted
+
+    
   }
   //CLEAR BUTTON
   /* Clear history function*/
-  
+
 
   
 });
